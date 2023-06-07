e12e18be8a8e7132d001a09a6b63b0972fb3ac0e
IngridTGit <ingrid.torres.ferreira@gmail.com>
2023-06-07 19:12:39 -0300

    Laços

--- VirtualFone/main.js
+++ VirtualFone/main.js
@@ -5,13 +5,38 @@ function valorPrimeiroBotao(){
 
 const listaTeclas = document.querySelectorAll
 ('input[type=button]');
+const display = document.querySelector('input[type=tel]');
 
-function valorBotaoUm(){
+function valorBotaoUmJanela(){
     alert(listaTeclas[0].value);
 }
 
 function valorBotaoUm(){
-    display.value += listaTeclas[0].value
+    display.value += listaTeclas[0].value;
 }
 
-listaTeclas[0].onclick = valorBotaoUm;
+function valorBotao(valor){
+   display.value += listaTeclas[valor].value;
+}
+
+/* let i = 0;
+while(i < listaTeclas.length){
+    const tecla = listaTeclas[i];
+    const indice = i;
+    tecla.onclick = function(){
+        valorBotao(indice);
+    }
+    i = i + 1;
+}  */
+
+for (let index = 0; index < listaTeclas.length; index++) {
+    const tecla = listaTeclas[index];
+    tecla.onclick = () => {
+        valorBotao(index);
+    }
+}
+
+
+function limpar(){
+    display.value = '';
+}
